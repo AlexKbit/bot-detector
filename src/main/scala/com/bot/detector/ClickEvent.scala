f0d9d42234418c7a1011c409ca757628ec469e22
Alexander Savchenko <sav4enkoaleksandr@gmail.com>
2019-07-09 21:54:04 +0300

    implement detection logic

--- src/main/scala/com/bot/detector/ClickEvent.scala
+++ src/main/scala/com/bot/detector/ClickEvent.scala
@@ -3,8 +3,19 @@ package com.bot.detector
 import java.util.Date
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 
 case class ClickEvent(@JsonProperty("unix_time") time: Date,
                       @JsonProperty("category_id") categoryId: Int,
                       @JsonProperty("ip") ip: String,
                       @JsonProperty("type") eventType:String)
+
+object ClickEvent {
+
+  val mapper = new ObjectMapper() with ScalaObjectMapper
+
+  def apply(value: String): ClickEvent = {
+    mapper.readValue[ClickEvent](value)
+  }
+}
--- src/main/scala/com/bot/detector/DetectorApp.scala
+++ src/main/scala/com/bot/detector/DetectorApp.scala
@@ -13,12 +13,47 @@ import com.datastax.spark.connector.streaming._
 object DetectorApp {
 
   val DEFAULT_MASTER = "local[*]"
+  val KAFKA_TOPIC = "click-stream"
+  val WHILE_BAN_TIME = 10 * 60 * 1000 // 10 min
+
+  val LIMIT_OF_EVENTS = 1000
+  val LIMIT_OF_CATEGORIES = 5
+  val LIMIT_OF_CLICK_VIEW = 3
 
   def main(args: Array[String]): Unit = {
     val master = if (args.length == 0) DEFAULT_MASTER else args(0)
-    val ss = createSparkSession(master)
-    val r = ss.sparkContext.parallelize(1 to 1000).map(Math.pow(_, 2)).sum()
-    println("Result: " + r)
+    val spark = createSparkSession(master)
+    import spark.implicits._
+    val clickStream = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094")
+      .option("subscribe", KAFKA_TOPIC)
+      .load()
+
+    val clickEvents: Dataset[ClickEvent] = clickStream
+      .selectExpr("CAST(value AS STRING)")
+      .map(r => ClickEvent(r.getString(0)))
+
+    val aggregates = clickEvents
+      .withWatermark("time", "60 seconds")
+      .groupBy(window($"time","10 minutes"), $"ip")
+      .agg(
+        count(col("eventType")).alias("eventsCount"),
+        count(col("eventType") === "click").alias("clickCount"),
+        count(col("eventType") === "view").alias("viewCount"),
+        count(col("categoryId")).alias("categoryCount"))
+      .where(s"eventsCount > $LIMIT_OF_EVENTS")
+      .where(s"clickCount / viewCount > $LIMIT_OF_CLICK_VIEW")
+      .where(s"categoryCount > $LIMIT_OF_CATEGORIES")
+
+    aggregates
+      .write
+      .mode(SaveMode.Overwrite)
+      .cassandraFormat("detected_bots", "botdetect")
+      .option("confirm.truncate", true)
+      .save
+
   }
 
   def createSparkSession(master: String): SparkSession = {
